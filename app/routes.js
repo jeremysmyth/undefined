954e8e29b36d97f0b6ce9eca00db57515112733a
Zakariyya Uddin <z.uddin@kainos.com>
2020-11-19 11:24:37 +0000

    Developed spike/proof of concept

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
 
-// Add your routes here - above the module.exports line
+const employeedata = require('./employeedata')
 
+// Add your routes here - above the module.exports line
+router.get('/list-employees', async (req, res) => {
+    res.render('list-employees', { employees: await employeedata.getEmployeesByDepartment()})
+})
 
 module.exports = router
